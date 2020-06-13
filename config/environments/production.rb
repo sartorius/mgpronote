832714264421164968505ae88461c2d2a09b1f7d
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@MacBook-Air-de-TsikyHarimino.local>
2020-06-13 09:25:24 +0300

    update prd mail

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -66,18 +66,20 @@ Rails.application.configure do
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  host = 'https://mysterious-atoll-47182.herokuapp.com'
+  host = 'https://pacific-bayou-41716.herokuapp.com'
   config.action_mailer.default_url_options = { host: host }
   ActionMailer::Base.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
+    :address        => 'mail34.lwspanel.com',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
+    #:user_name      => ENV['SENDGRID_USERNAME'],
+    #:password       => ENV['SENDGRID_PASSWORD'],
+    :user_name      => 'ne-pas-repondre@projetbannister.com',
+    :password       => 'NePasRepondr_12',
+    :domain         => 'projetbannister.com',
     :enable_starttls_auto => true
   }
-  
+
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
   config.i18n.fallbacks = true
