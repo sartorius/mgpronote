4e69d0bf64c813f3121dca92a76376dad8a43ccf
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@MacBook-Air-de-TsikyHarimino.local>
2020-06-14 20:00:57 +0300

    test cam2

--- app/javascript/custmain_mg.js
+++ app/javascript/custmain_mg.js
@@ -1,10 +1,6 @@
 $(document).ready(function() {
   console.log('We are in MGFollowAPP JS2');
 
-  const firstDeviceId = videoInputDevices[0].deviceId;
-  codeReader
-    .decodeOnceFromVideoDevice(firstDeviceId, 'video')
-    .then(result => console.log(result.text))
-    .catch(err => console.error(err));
+  
 
 });
