b15b6acd73f9d2349699cd1fe568ee877e60bc1e
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@MacBook-Air-de-TsikyHarimino.local>
2020-06-15 12:41:14 +0300

    reload turbolinks

--- app/javascript/custmain_mg.js
+++ app/javascript/custmain_mg.js
@@ -9,15 +9,23 @@ $(document).ready(function() {
   }
 });
 
+$(document).on('turbolinks:load', function() {
+  // Should be called at each visit
+  console.log('TBL Page as changed !');
+  if($('#mg-graph-identifier').text() == 'readbc'){
+    loadCameraRead();
+  }
+})
+
 function loadCameraRead(){
   let selectedDeviceId;
   const codeReader = new ZXing.BrowserBarcodeReader();
-  //console.log('ZXing code reader initialized');
-  alert('ZXing code reader initialized');
+  console.log('ZXing code reader initialized');
   codeReader.getVideoInputDevices()
       .then((videoInputDevices) => {
           const sourceSelect = document.getElementById('sourceSelect')
           selectedDeviceId = videoInputDevices[0].deviceId
+
           if (videoInputDevices.length > 1) {
               videoInputDevices.forEach((element) => {
                   const sourceOption = document.createElement('option')
