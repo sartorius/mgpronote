c5a6ac8bc794016b3b499fd2529fb6392f0819f8
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@AirdeTsHarimino.Dlink>
2020-06-22 19:13:38 +0300

    remove padding

--- app/javascript/partnermng.js
+++ app/javascript/partnermng.js
@@ -114,7 +114,8 @@ function display12Cb(){
   numberRow = $('#nb-lines').html();
   console.log("Read number of row: " + numberRow);
   for(i=0; i<numberRow; i++){
-    JsBarcode("#mbc-"+i, $("#item-bc-"+i).html().padStart(9, 'M') );
+    // Use this to pad
+    JsBarcode("#mbc-"+i, $("#item-bc-"+i).html());
   }
 }
 
