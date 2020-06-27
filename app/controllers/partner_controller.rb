cb091e7b2b57e2cf9433d1baa064fc2268344958
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@AirdeTsHarimino.Dlink>
2020-06-27 12:53:51 +0300

    display company name

--- app/controllers/partner_controller.rb
+++ app/controllers/partner_controller.rb
@@ -1,5 +1,6 @@
 class PartnerController < ApplicationController
   before_action :mgs_user_is_partner
+  before_action :get_partner_company_name
 
 
   def reception
@@ -9,8 +10,10 @@ class PartnerController < ApplicationController
   def mainstatistics
     sql_query = "SELECT rs.step, COUNT(1) AS cnt_stat " +
                         " FROM barcode bc JOIN ref_status rs ON bc.status = rs.id " +
+                        # You can use this to make sure barcode is link to the partner
                         " JOIN users u ON u.partner = bc.partner_id " +
                         " WHERE u.id = " + @current_user.id.to_s +
+                        # End partner check
         	              " GROUP BY rs.step;"
     begin
 
@@ -33,7 +36,12 @@ class PartnerController < ApplicationController
   def dashboard
 
     sql_query = "SELECT bc.id AS id, bc.ref_tag AS ref_tag, rs.step AS step, LPAD(bc.secret_code::CHAR(4), 4, '0') AS bsecret_code " +
-                      "FROM barcode bc join ref_status rs on rs.id = bc.status ORDER BY bc.id ASC;"
+                      " FROM barcode bc join ref_status rs on rs.id = bc.status " +
+                      # You can use this to make sure barcode is link to the partner
+                      " JOIN users u ON u.partner = bc.partner_id " +
+                      " WHERE u.id = " + @current_user.id.to_s +
+                      # End partner check
+                      " ORDER BY bc.id ASC;"
 
 
     begin
@@ -53,7 +61,12 @@ class PartnerController < ApplicationController
   def printtwelve
 
     sql_query = "SELECT bc.id AS id, bc.ref_tag AS ref_tag, rs.step AS step, LPAD(bc.secret_code::CHAR(4), 4, '0') AS bsecret_code " +
-                      "FROM barcode bc join ref_status rs on rs.id = bc.status ORDER BY bc.id ASC LIMIT 12;"
+                      "FROM barcode bc join ref_status rs on rs.id = bc.status " +
+                      # You can use this to make sure barcode is link to the partner
+                      " JOIN users u ON u.partner = bc.partner_id " +
+                      " WHERE u.id = " + @current_user.id.to_s +
+                      # End partner check
+                      " ORDER BY bc.id ASC LIMIT 12;"
 
 
     begin
@@ -72,5 +85,18 @@ class PartnerController < ApplicationController
 
   def printnotrack
     render 'printnotrack'
+  end
+
+  private
+
+  def get_partner_company_name
+    unless !@resultSetCompany.nil?
+      sql_query = "SELECT name, description, to_phone, website FROM ref_partner WHERE id = " + @current_user.partner.to_s + ";"
+      begin
+        #flash[:info] = "Step save: " + params[:stepstep] + " /" + params.to_s + " //" + sql_query
+        #@resultSet = ActiveRecord::Base.connection.execute(sql_query)
+        @resultSetCompany = ActiveRecord::Base.connection.exec_query(sql_query).cast_values
+      end
+    end
   end
 end
