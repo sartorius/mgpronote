24a19bbabf60a0d0a6f782bdf9daa5df7f826791
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@AirdeTsHarimino.Dlink>
2020-07-01 08:27:04 +0300

    block mobile link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -23,7 +23,13 @@ module ApplicationHelper
     return readArray.to_s
 
   end
-  
+
+  def mobile_device
+    agent = request.user_agent
+    return "tablet" if agent =~ /(tablet|ipad)|(android(?!.*mobile))/i
+    return "mobile" if agent =~ /Mobile/
+    return "desktop"
+  end
 
   # Returns the full title on a per-page basis.
   def full_title(page_title = '')
