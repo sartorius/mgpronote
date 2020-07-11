af7cbe6f47f078c1f3d010f7446a89db2befd07f
Tsikyharimino Rakotoarinia Ratinahirana <herinirinaratinahirana@AirdeTsHarimino.Dlink>
2020-07-11 17:08:25 +0300

    add fontawesome

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -18,6 +18,7 @@ require("clientmng.js")
 require("persoresel.js")
 require("turbolinks").start()
 import "bootstrap"
+import "@fortawesome/fontawesome-free/js/all"
 
 
 // Uncomment to copy all static images under ../images to the output folder and reference
