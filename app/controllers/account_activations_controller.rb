374f046e1b8fd04afc0a0174f99a29010c26f43a
sartorius <ratinahirana@gmail.com>
2020-08-09 03:26:09 +0200

    upd activation

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -2,13 +2,20 @@ class AccountActivationsController < ApplicationController
 
   def edit
     user = User.find_by(email: params[:email])
-    if user && !user.activated? && user.authenticated?(:activation, params[:id])
+
+    if user.nil?
+      flash[:danger] = "Cet email est introuvable."
+      redirect_to root_url
+    elsif user.activated?
+      flash[:success] = "Bonne nouvelle, ce compte est déjà activé. Connectez-vous s'il vous plaît."
+      redirect_to login_url
+    elsif user && !user.activated? && user.authenticated?(:activation, params[:id])
       user.activate
       log_in user
-      flash[:success] = "Account activated!"
+      flash[:success] = "Votre compte est activé !"
       redirect_to user
     else
-      flash[:danger] = "Invalid activation link"
+      flash[:danger] = "Le lien d'activation est invalide."
       redirect_to root_url
     end
   end
