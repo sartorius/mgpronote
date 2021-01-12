53883d2295093d9f2d81f55617ad28e7a6df3274
RATINAHIRANA Herinirina <ratinahirana@MacBook-Pro-de-RATINAHIRANA.local>
2021-01-12 23:26:38 +0100

    correct comment

--- config/db/session.sql
+++ config/db/session.sql
@@ -1,14 +1,5 @@
 
 
--- Delete a BC
-
-select * from barcode bc where bc.status = 2
-
-select * from wk_tag wt where wt.current_step_id = 2;
-
-select * from wk_tag_com wtc where wtc.wk_tag_id IN (
-	select id from wk_tag wt where wt.current_step_id = 2
-);
 
 INSERT INTO ref_workflow (id, code, description, mode) VALUES (7, 'MA', 'Tana manufacture', 'M');
 
